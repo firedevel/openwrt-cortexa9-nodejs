db0fa30061961fb524cd850642877ab39607ac09
Hi-hass <70802618+Hi-hass@users.noreply.github.com>
2022-05-11 08:33:23 +0800

    Fix service

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,11 +1,14 @@
 npm install -g homebridge homebridge-config-ui-x homebridge-miot
 hb-service install --user root -U /root/homebridge
-echo '#!/bin/sh
+echo '#!/bin/sh /etc/rc.common
+
+START=99
+
 start(){
 homebridge -U /root/homebridge >> /root/homebridge/last.log
 }
 ' >> /etc/init.d/homebridge
-
-ln -s /etc/init.d/homebridge >> /etc/rc.d/S98homebridge
+chmod +wrx /etc/init.d/homebridge
+ln -s /etc/init.d/homebridge /etc/rc.d/S98homebridge
 /etc/rc.d/S98homebridge enable
 reboot
